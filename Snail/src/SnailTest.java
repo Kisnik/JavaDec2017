c5d2e6b323c1ac94f90bd06453ffd92907ed1ad1
Kisnik <kisnikn@mail.ru>
2018-02-14 23:06:10 +0300

    added new task

---
+++ Snail/src/SnailTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Created by Kisnik on 12.02.2018.
+ */
+public class SnailTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int n = in.nextInt();
+        System.out.println("Enter the number of columns");
+        int m = in.nextInt();
+        if (n==0 || m==0) { //check of  array dimension
+            System.out.println("Error, wrong value");
+            return;
+        }
+        int arr[][] = new int[n][m]; //enter an array
+        for(int i=0; i < n; i++){
+            System.out.println("Enter the row number " + (i+1) + ": ");
+            for(int j=0; j < m; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        Snail testArray = new Snail();
+        testArray.snailArray(arr); //out af the array by snail
+    }
+}
--- SquareArea/src/AreaTest.java
+++ SquareArea/src/AreaTest.java
@@ -5,20 +5,21 @@ import java.util.*;
 public class AreaTest {
     public static void main(String[] args) {
         Rectangles a = new Rectangles(); //creation of rectangle
-        double[] a1 = {2, 0}; //corrs  of the first point
-        double[] a2 = {5, 2}; //corrs  of the second point
+        double[] a1 = {0, 3}; //corrs  of the first point
+        double[] a2 = {3, 4}; //corrs  of the second point
         a.setCorr1(a1);
         a.setCorr2(a2);
         double S1 = a.RectanglesArea(); //area of rectangle
         System.out.println("The area of the first rectangle is: " + S1);
         Rectangles b = new Rectangles();
-        double[] b1 = {1, 1}; //corrs  of the first point (second rectangle)
-        double[] b2 = {4, 4}; //corrs  of the second point (second rectangle)
+        double[] b1 = {0, 0}; //corrs  of the first point (second rectangle)
+        double[] b2 = {4, 2}; //corrs  of the second point (second rectangle)
         b.setCorr1(b1);
         b.setCorr2(b2);
         double S2 = b.RectanglesArea();
-        System.out.println("The area of the first rectangle is: " + S2);
+        System.out.println("The area of the second rectangle is: " + S2);
         double S3 = b.RectanglesCrossSection(a, b);
         System.out.println("The area of the cross section of rectangles is: " + S3);
     }
 }
+
