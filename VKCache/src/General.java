d9dba179f5f3c25759a6385c0f35e0c2e0656dea
Kisnik <35270226+Kisnik@users.noreply.github.com>
2018-02-27 21:09:52 +0300

    Update General.java
    Added the check if file is mp3

--- VKCache/src/General.java
+++ VKCache/src/General.java
@@ -9,17 +9,27 @@ public class General {
         Path fromPath = Paths.get("C:\\Users\\Kisnik\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Media Cache");
         DirectoryStream<Path> stream = Files.newDirectoryStream(fromPath);
         for (Path file: stream) {
-                Path toPathFile = Paths.get("D:\\MusicCache\\" + file.getFileName() + ".mp3");
-                if (!Files.exists(toPathFile)) {
-                    Files.createFile(toPathFile);
-                }
-                try {
-                    Files.copy(file, toPathFile, REPLACE_EXISTING);
-                } catch (IOException e) {
-                    e.printStackTrace();
+            String s = String.valueOf(file.getFileName());
+            if (s.charAt(0)=='f') {// if file name starts from char "f" it is mp3 file
+                //here is the alternative check if the file is mp3: we are looking if the input of the file befins from ID3.
+                //if yes - it is mp3, if no - it is not mp3. But we have found the cases when the input of the file is not begins from ID3, but it is mp3 file
+                //this is the reason, why this check was commented
+                /*BufferedReader br = new BufferedReader((new FileReader(fromPath + "\\" + s)));
+                String line = br.readLine();
+                if (line.startsWith("ID3")) {*/
+                    Path toPathFile = Paths.get("D:\\MusicCache\\" + file.getFileName() + ".mp3");
+                    if (!Files.exists(toPathFile)) {
+                        Files.createFile(toPathFile);
+                    }
+                    try {
+                        Files.copy(file, toPathFile, REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        e.printStackTrace();
 
-                }
+                    }
+                /*}*/
             }
+        }
 
     }
 }
