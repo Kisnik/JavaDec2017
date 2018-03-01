9bd115486c109d9529bdeb8ec45dfb488d554a02
Kisnik <35270226+Kisnik@users.noreply.github.com>
2018-03-01 12:17:23 +0300

    UPD general class

--- VKCache/src/General.java
+++ VKCache/src/General.java
@@ -1,35 +1,52 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.*;
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 
 public class General {
     public static void main(String[] args) throws IOException {
-        Path toPath = Paths.get("D:\\MusicCache");
-        Files.createDirectory(toPath);
-        Path fromPath = Paths.get(Paths.get(System.getProperty("user.home"), "AppData", "Local", "Google", "Chrome", "User Data", "Default", "Media Cache").toString());
-        DirectoryStream<Path> stream = Files.newDirectoryStream(fromPath);
-        for (Path file: stream) {
-            String s = String.valueOf(file.getFileName());
-            if (s.charAt(0)=='f') {// if file name starts from char "f" it is mp3 file
-                //here is the alternative check if the file is mp3: we are looking if the input of the file befins from ID3.
-                //if yes - it is mp3, if no - it is not mp3. But we have found the cases when the input of the file is not begins from ID3, but it is mp3 file
-                //this is the reason, why this check was commented
-                /*BufferedReader br = new BufferedReader((new FileReader(fromPath + "\\" + s)));
-                String line = br.readLine();
-                if (line.startsWith("ID3")) {*/
-                    Path toPathFile = Paths.get("D:\\MusicCache\\" + file.getFileName() + ".mp3");
-                    if (!Files.exists(toPathFile)) {
-                        Files.createFile(toPathFile);
-                    }
-                    try {
-                        Files.copy(file, toPathFile, REPLACE_EXISTING);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-                    }
-                /*}*/
-            }
+        System.out.println("Choose your browser:");
+        System.out.println("1 - Google Chrome");
+        System.out.println("2 - Opera");
+        System.out.println("3 - Mozilla FireFox");
+        System.out.println("4 - Yandex browser");
+
+        int n = Integer.parseInt(reader.readLine());
+
+        Path toPath = Paths.get("D:\\MusicCache1");
+        if (!Files.exists(toPath)) Files.createDirectory(toPath);// creating target path
+
+        Path fromPathChrome = Paths.get(Paths.get(System.getProperty("user.home"), "AppData", "Local", "Google", "Chrome", "User Data", "Default", "Media Cache")
+                .toAbsolutePath().toString());
+        Path fromPathOpera = Paths.get(Paths.get(System.getProperty("user.home"), "AppData", "Local", "Opera Software", "Opera Stable", "Media Cache")
+                .toAbsolutePath().toString());
+        Path fromPathMozilla = Paths.get(Paths.get(System.getProperty("user.home"), "Local Settings", "Application Data", "Mozilla", "Firefox",
+                "Profiles", "zayl0w0i.default", "cache2", "entries").toAbsolutePath().toString());
+        Path fromPathYa = Paths.get(Paths.get(System.getProperty("user.home"), "AppData", "Local", "Yandex", "YandexBrowser", "User Data", "Default", "Media Cache")
+                .toAbsolutePath().toString());
+
+        switch (n){
+            case 1:
+                ForChromium.getMusic(fromPathChrome, toPath);// Chrome
+                break;
+            case 2:
+                ForChromium.getMusic(fromPathOpera, toPath);//Opera
+                break;
+            case 3:
+                ForMozilla.getMusic(fromPathMozilla, toPath);// Mozilla
+                break;
+            case 4:
+                ForChromium.getMusic(fromPathYa, toPath);//Yandex
+                break;
+            default:
+                System.out.println("Incorrect input");
+                break;
+
         }
+
 
     }
 }
