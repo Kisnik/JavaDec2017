9798b6e75315780b3841afa24eea200caec75d99
Kisnik <kisnikn@mail.ru>
2018-03-09 12:59:58 +0300

    Added changes in VKCache app

---
+++ VKCache/src/ForChromium.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.*;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+public class ForChromium {
+    public static void getMusic(Path fromPath, Path toPath){
+        String header = "";
+
+        try {
+            DirectoryStream<Path> stream = Files.newDirectoryStream(fromPath);
+
+            Path toPathFile = null;
+            for (Path file : stream) {
+                String s = String.valueOf(file.getFileName());
+
+                if (s.charAt(0) == 'f') {//this apply getting files without closing browser
+                    //System.out.println(String.valueOf(file.getFileName()));
+                    BufferedReader br = new BufferedReader((new FileReader(fromPath + "\\" + s)));
+                    String line = br.readLine();
+
+
+
+                    if (line.startsWith("ID3") || line.startsWith("TAG")) {
+                        toPathFile = Paths.get(toPath + "\\" + file.getFileName() + ".mp3");
+                        header = String.valueOf(file.getFileName()) + ".mp3";
+                        if (!Files.exists(toPathFile)) {
+                            Files.createFile(toPathFile);// creating target dir
+                        }
+                        try {
+                            Files.copy(file, toPathFile, REPLACE_EXISTING);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+
+                        }
+                    } else {
+                        if (toPathFile != null) {
+                            FileInputStream fin=new FileInputStream(String.valueOf(file));
+                            byte[] data = new byte[fin.available()];
+                            fin.read(data, 0, fin.available());
+                            Files.write(toPathFile, data, StandardOpenOption.APPEND);
+                        }
+                    }
+                }
+            }
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+    }
+}
