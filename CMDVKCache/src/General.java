d64d4b295220790ec382ffa18d491606355a07f8
Kisnik <kisnikn@mail.ru>
2018-03-12 12:39:55 +0300

    added description

--- CMDVKCache/src/General.java
+++ CMDVKCache/src/General.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.*;
 
-
+//working on Windows
 public class General {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
